d3f69165922c3b7d9181eddaa01e8360d6a71495
Elena_Syurtukova <evasineva@bk.ru>
2025-04-13 15:24:55 +0900

    test_product, main

--- tests/test_product.py
+++ tests/test_product.py
@@ -1,5 +1,24 @@
+from src.product import Product
+
 def test_product_init(product):
     assert product.name == "дюшес"
     assert product.description == "леденцы"
     assert product.price == 1.5
     assert product.quantity == 10
+
+def test_product_new_product():
+    product = Product.new_product({"name": "белый хлеб",
+                       "description": "хлебобулочные изделия",
+                       "price": 35.5,
+                       "quantity": 10})
+    assert product.name == "белый хлеб"
+    assert product.description == "хлебобулочные изделия"
+    assert product.price == 35.5
+    assert product.quantity == 10
+
+def test_price_setter(capsys, product):
+    product.price = -15.5
+    message = capsys.readouterr()
+    assert message.out.strip() == "Цена не должна быть нулевая или отрицательная"
+    product.price = 15.5
+    assert product.price == 15.5
